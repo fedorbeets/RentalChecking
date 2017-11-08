68eb5fc43faa25aa3ed82371fc93d0d670724c59
Fedor <fedorbeets@protonmail.com>
2017-11-08 10:57:54 +0100

    runs without crashing, still wrong result

--- EqualityTest.py
+++ EqualityTest.py
@@ -1,5 +1,9 @@
 import py_ecc.optimized_bn128.optimized_pairing as pairing
+from py_ecc.optimized_bn128.optimized_pairing import multiply
+from py_ecc.optimized_bn128.optimized_pairing import add
 import random
+import numpy as np
+from operator import mul
 
 # Picking key_space as all numbers 0...pairing.curve_order, which seems like a suitably large number.
 key_space = pairing.curve_order
@@ -22,11 +26,11 @@ def setup(n):
         # random number in key space
         beta_x = rand.randint(1, key_space)
         # put elements on curve
-        elliptic_alpha_x = pairing.multiply(pairing.G2, alpha_x)
-        elliptic_gamma_x = pairing.multiply(pairing.G2, gamma_x)
+        elliptic_alpha_x = multiply(pairing.G2, alpha_x)
+        elliptic_gamma_x = multiply(pairing.G2, gamma_x)
         msks.append(dict(
             [('alpha_multiplied', elliptic_alpha_x), ('beta', beta_x), ('gamma_multiplied', elliptic_gamma_x)]))
-        g1_alpha_x = pairing.multiply(pairing.G1, alpha_x)
+        g1_alpha_x = multiply(pairing.G1, alpha_x)
         chck_keys.append(dict([('alpha_g1', g1_alpha_x), ('beta', beta_x), ('gamma', gamma_x)]))
     return msks, chck_keys
 
@@ -38,16 +42,18 @@ def setup(n):
 # n - length of rule vector
 def gen_token(msks, rule, n):
     rand = random.SystemRandom()
-    total = 1
+    # identity element for addition
+    total = (pairing.FQ2.zero(), pairing.FQ2.zero(), pairing.FQ2.zero())
     mini_tokens = []
     for x in range(0, n):
         u_x = rand.randint(1, pairing.curve_order)
-        u_x_multiplied = pairing.multiply(pairing.G2, u_x)
+        u_x_multiplied = multiply(pairing.G2, u_x)
         exponent = u_x * prp(msks[x]['beta'], rule[x])
-        eliptic_prp = pairing.multiply(msks[x]['alpha_multiplied'], exponent)
+        elliptic_prp = multiply(msks[x]['alpha_multiplied'], exponent)
         assert isinstance(u_x, int)
-        total *= pairing.multiply(msks['gamma_multiplied'], u_x)
-        mini_tokens.append(dict([('u_multiplied', u_x_multiplied), ('prp_multiplied', eliptic_prp)]))
+        # todo: assume multiplication of elliptic curve elements (big sum-multiply pi symbol) means add all elements together
+        total = add(total, multiply(msks[x]['gamma_multiplied'], u_x))
+        mini_tokens.append(dict([('u_multiplied', u_x_multiplied), ('prp_multiplied', elliptic_prp)]))
     return mini_tokens, total
 
 
@@ -57,12 +63,12 @@ def gen_token(msks, rule, n):
 def encrypt(chck_key, identifier, message):
     rand = random.SystemRandom()
     r_x = rand.randint(1, pairing.curve_order)
-    r_x_multiplied = pairing.multiply(pairing.G1, r_x)
+    r_x_multiplied = multiply(pairing.G1, r_x)
     exponent = r_x * prp(chck_key['beta'], message)
-    g1_exponentiated = pairing.multiply(chck_key['alpha_g1'], exponent)
-    identifier_hashed_multiplied = pairing.multiply(elliptic_hash(identifier), message)
+    g1_exponentiated = multiply(chck_key['alpha_g1'], exponent)
+    identifier_hashed_multiplied = multiply(elliptic_hash(identifier), message)
     # TODO: in this step they have to be added together, right?
-    g1_and_hash_added = pairing.add(g1_exponentiated, identifier_hashed_multiplied)
+    g1_and_hash_added = add(g1_exponentiated, identifier_hashed_multiplied)
     return dict([('hash_ident', elliptic_hash(identifier)), ('r_x_mul', r_x_multiplied),
                 ('g1_hash_add', g1_and_hash_added)])
 
@@ -70,25 +76,28 @@ def encrypt(chck_key, identifier, message):
 # off chain test function
 def test(token, n, *arg):
     assert len(arg) == n
-    # pairing with 'complicated token element'
-    check_total = 1
+    # pairing with complicated element in g1
+    check_total = pairing.FQ12.zero()
     for x in range(0, n):
-        paired = pairing.pairing(arg[x]['g1_hash_add'], token[0][x]['u_multiplied'])
+        paired = pairing.pairing(token[0][x]['u_multiplied'], arg[x]['g1_hash_add'])
         assert isinstance(paired, pairing.FQ12)
-        check_total *= paired
-    token_total = 1
+        check_total = check_total.__add__(paired)
+    # pairing with complicated element in g2
+    token_total = pairing.FQ12.zero()
     for x in range(0, n):
-        paired = pairing.pairing(arg[x]['r_x_mul'], token[0][x]['prp_multiplied'])
-        token_total *= paired
-    # TODO; assuming only multiplying pairing(hash,token_total) once at end
-    token_total *= pairing.pairing(arg[0]['hash_ident'], token[1])
+        paired = pairing.pairing(token[0][x]['prp_multiplied'], arg[x]['r_x_mul'])
+        token_total = token_total.__add__(paired)
+    # TODO; assuming only multiplying pairing(hash,token_total) once at end instead of every time in loop
+    token_total = token_total.__add__(pairing.pairing(token[1], arg[0]['hash_ident']))
     # TODO: assuming equality will check all individual exponents
+    print("check_total: ", check_total)
+    print("token_total: ", token_total)
     return check_total == token_total
 
 
 # pseudo-random permutation function, stub implementation
 # todo: make non-stub implementation
-def prp(beta, y):
+def prp(beta: int, y: int) -> int:
     return 5
 
 
@@ -96,9 +105,21 @@ def prp(beta, y):
 # todo: secure implementation of actual one way function
 def elliptic_hash(number: int) -> tuple:
     modded_number = number % pairing.curve_order
-    return pairing.multiply(pairing.G1, modded_number)
+    return multiply(pairing.G1, modded_number)
 
 
-setup = setup(3)
-print(setup[0][0]['alpha_multiplied'])
-#print(setup[1])
+#print("pretest")
+
+
+print("Setup")
+master_keys, check_keys = setup(3)
+print("GenToken")
+test_token = gen_token(master_keys, [1, 1, 1], 3)
+print("encrypt checks")
+identify = 5
+check1 = encrypt(check_keys[0], identify, 1)
+check2 = encrypt(check_keys[1], identify, 1)
+check3 = encrypt(check_keys[2], identify, 1)
+print("Check")
+result = test(test_token, 3, check1, check2, check3)
+print(result)
