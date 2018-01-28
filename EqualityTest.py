7fa83fce1526fc2dacc35f9f3b5af9350130508d
Fedor <fedorbeets@protonmail.com>
2018-01-28 13:22:22 +0100

    Added multithreading for 50% speedup

--- EqualityTest.py
+++ EqualityTest.py
@@ -3,6 +3,11 @@ from py_ecc.optimized_bn128.optimized_pairing import multiply, add
 from py_ecc.optimized_bn128.optimized_pairing import normalize1, normalize
 import random
 import math
+import cProfile
+from multiprocessing import Pool
+from functools import reduce
+import operator
+
 
 # Picking key_space as all numbers 0...pairing.curve_order, which seems like a suitably large number.
 key_space = pairing.curve_order
@@ -15,36 +20,47 @@ MESSAGE_SPACE = 32  # Don't make 1 due to log function
 # and check keys CK 1 through CK n . Public parameters part of the library
 def setup(n, alpha=0, gamma=0, beta=0):  # optional rng determination for testing purposes NOT CRYPTOGRAPHICALLY SAFE
     rand = random.SystemRandom()
-    msks = []       # master secret key for token generation
-    chck_keys = []  # checking keys
-    for _ in range(n):
-        single_msk = {}  # a part of the master secret key
-        single_chck = {}  # a single checking key
-        # generate random numbers of Zp*
-        alpha_x = rand.randint(1, pairing.curve_order)
-        if alpha != 0:  # if random number pregiven
-            alpha_x = alpha
-        gamma_x = rand.randint(1, pairing.curve_order)
-        if gamma != 0:
-            gamma_x = gamma
-        # Key space amount of random numbers
-        beta_x = []
-        for __ in range(math.ceil(math.log(MESSAGE_SPACE, 2))):
-            beta_x.append(rand.randint(1, pairing.curve_order))
-        if beta != 0:  # if random number has been given
-            beta_x = beta
+    pool = Pool()
 
-        single_msk['alpha_multiplied'] = multiply(pairing.G2, alpha_x)  # alpha_x * G2
-        single_msk['beta'] = beta_x
-        single_msk['gamma_multiplied'] = multiply(pairing.G2, gamma_x)  # gamma_x * G2
-        msks.append(single_msk)
+    # 3 random values, master secret keys for token generation, checking keys
+    # alphas, gammas have a list of random values
+    # betas has a list of a list of random values
+    alphas, gammas, betas, msks, chck_keys = [[] for _ in range(5)]
 
-        single_chck['alpha_g1'] = multiply(pairing.G1, alpha_x)
-        single_chck['beta'] = beta_x
-        single_chck['gamma_x'] = gamma_x
-        chck_keys.append(single_chck)
+    # rng setup
+    if alpha != 0:  # if random number pregiven
+        assert(gamma != 0 and beta != 0)
+        alphas = alpha
+        gammas = gamma
+        betas = beta
+    else:
+        for _ in range(n):
+            alphas.append(rand.randint(1, pairing.curve_order))
+            gammas.append(rand.randint(1, pairing.curve_order))
+            # Key space amount of random numbers
+            beta_x = []
+            for __ in range(math.ceil(math.log(MESSAGE_SPACE, 2))):
+                beta_x.append(rand.randint(1, pairing.curve_order))
+            betas.append(beta_x)
+
+    result = pool.starmap(single_setup, zip(alphas, gammas, betas))
+    msks, chck_keys = list(zip(*result))
+
     return msks, chck_keys
 
+
+def single_setup(alpha=0, gamma=0, beta=0):
+    msk = {}  # a part of the master secret key
+    chck = {}  # a single checking key
+
+    msk['alpha_multiplied'] = multiply(pairing.G2, alpha)  # alpha_x * G2
+    msk['beta'] = beta
+    msk['gamma_multiplied'] = multiply(pairing.G2, gamma)  # gamma_x * G2
+
+    chck['alpha_g1'] = multiply(pairing.G1, alpha)
+    chck['beta'] = beta
+    chck['gamma_x'] = gamma
+    return msk, chck
 
 # Takes a list of master keys and a list of rule requirements and outputs an encrypted token that
 # check results are to be matched against
@@ -52,23 +68,29 @@ def setup(n, alpha=0, gamma=0, beta=0):  # optional rng determination for testin
 # rule - list of n numbers in "message space"
 # n - length of rule vector
 def gen_token(msks, rule, n, u=0):
+    assert(len(rule) == n)
     rand = random.SystemRandom()
-    # identity element for addition
-    total = (pairing.FQ2.zero(), pairing.FQ2.zero(), pairing.FQ2.zero())
-    mini_tokens = []
-    for x in range(n):
-        single_token = {}  # the values of a token for a single x
-        u_x = rand.randint(1, pairing.curve_order)
-        if u != 0:  # if random number has been given, use it
-            u_x = u
+    pool = Pool()
 
-        single_token['u_multiplied'] = multiply(pairing.G2, u_x)
+    if u != 0:  # rng predetermination
+        us = u
+    else:
+        us = [rand.randint(1, pairing.curve_order) for _ in range(n)]
 
-        exponent = (prp(msks[x]['beta'], rule[x]) * u_x) % pairing.curve_order
-        single_token['alpha_mul_g2_mul_prp'] = multiply(msks[x]['alpha_multiplied'], exponent)
-        mini_tokens.append(single_token)
-        total = add(total, multiply(msks[x]['gamma_multiplied'], u_x))
+    result = pool.starmap(single_token_fun, zip(msks, rule, us))
+    mini_tokens, u_multiplieds = list(zip(*result))
+    total = reduce(add, u_multiplieds)  # add all u values together
+
     return mini_tokens, total
+
+
+def single_token_fun(msk, rule, u):
+    single_token = {}
+    single_token['u_multiplied'] = multiply(pairing.G2, u)
+
+    exponent = (prp(msk['beta'], rule) * u) % pairing.curve_order
+    single_token['alpha_mul_g2_mul_prp'] = multiply(msk['alpha_multiplied'], exponent)
+    return single_token, multiply(msk['gamma_multiplied'], u)
 
 
 # chck_key - 1 of the check keys from setup
@@ -95,28 +117,29 @@ def encrypt(chck_key, identifier, message, r=0):
 # off chain test function
 def test(token, n, *arg):
     assert len(arg) == n
-    # pairing with complicated element in g1
-    check_total = pairing.FQ12.one()
-    for x in range(n):
-        paired = pairing.pairing(token[0][x]['u_multiplied'], arg[x]['g1_mul_prp_add_ident_hash'])
-        check_total = check_total * paired
-        # print values so I can test blockchain function
-        print("g2toU: ", pairing.normalize(token[0][x]['u_multiplied']))
-        print("g1toalpha: ", pairing.normalize(arg[x]['g1_mul_prp_add_ident_hash']))
+    pool = Pool()
+    left_arguments = []
 
-    # pairing with complicated element in g2
-    token_total = pairing.FQ12.one()
+    # create iterable from the arguments, so that starmap can be used
     for x in range(n):
-        paired = pairing.pairing(token[0][x]['alpha_mul_g2_mul_prp'], arg[x]['r_x_mul'])
-        token_total = token_total * paired
-        # print values so I can test blockchain function
-        print("g2toalpha: ", pairing.normalize(token[0][x]['alpha_mul_g2_mul_prp']))
-        print("g1toRX: ", pairing.normalize(arg[x]['r_x_mul']))
-    token_total = token_total * pairing.pairing(token[1], arg[0]['hash_ident'])
-    #print values so I can test blockchain function
-    print("total: ", pairing.normalize(token[1]))
-    print("hashID: ", pairing.normalize(arg[0]['hash_ident']))
+        left_arguments.append([token[0][x]['u_multiplied'], arg[x]['g1_mul_prp_add_ident_hash']])
 
+    # pairings where g2 = G2^u  and g1 = G1^alpha*prp(beta,x)*r * hash(id)^gamma
+    g2_u_g1_prp = pool.starmap(pairing.pairing, left_arguments)  # do all pairings at the same time
+    check_total = reduce(operator.mul, g2_u_g1_prp)  # multiply pairings together
+
+    right_arguments = []
+    for x in range(n):
+        # pairing with g2 = G2^alpha*prp(beta,y)*u  and g1 = G1^r
+        right_arguments.append([token[0][x]['alpha_mul_g2_mul_prp'], arg[x]['r_x_mul']])
+
+    # The pairing where g2 = mulsum(G2^gamma*u) and g1 = hash(ID)
+    right_arguments.append([token[1], arg[0]['hash_ident']])
+    g2_alpha_g1_r = pool.starmap(pairing.pairing, right_arguments)
+    token_total = reduce(operator.mul, g2_alpha_g1_r)
+
+    # to print values for blockchain function:
+    # print("thingie: ", pairing.normalize( ---location--- ))
     return check_total == token_total
 
 
@@ -124,7 +147,6 @@ def test(token, n, *arg):
 def prp(b: list, x: int) -> int:
     output = 1
     assert len(b) <= MESSAGE_SPACE
-    # todo: starting at 0, because I go from 0..n-1 instead of 1..n
     # could be optimized to go to range min(math.ceil(math.log(x, 2))), 1)
     for i in range(len(b)):  # prf starts at 1, 0th is pairing.multiply(g1,alpha_i)
         bit_i = ((x >> i) & 1)
@@ -142,23 +164,21 @@ def elliptic_hash(number: int) -> tuple:
 
 def test_routine():
     print("Setup")
-    master_keys, check_keys = setup(1, alpha=2, gamma=2, beta=[3, 4])
+    master_keys, check_keys = setup(5)
     print("GenToken")
-    # note: rng for u is re-used in every rule-loop, that is not realistic for system
-    test_token = gen_token(master_keys, [1], 1, u=5)
+    test_token = gen_token(master_keys, [3,3,3,3,3], 5)
     print("encrypt checks")
     identify = 5
-    check1 = encrypt(check_keys[0], identify, 1, r=6)
-    # check2 = encrypt(check_keys[1], identify, 1, r=4708642907422499050704862076428092405590089492351158641052908492133734418081)
-    # check3 = encrypt(check_keys[2], identify, 1, r=10465556228797287385848073339865451232298354941118980474002801677313279097215)
+    check1 = encrypt(check_keys[0], identify, 3)
+    check2 = encrypt(check_keys[1], identify, 3)
+    check3 = encrypt(check_keys[2], identify, 3)
+    check4 = encrypt(check_keys[3], identify, 3)
+    check5 = encrypt(check_keys[4], identify, 3)
     print("Check")
-    #print(check1)
-    result = test(test_token, 1, check1)
+    result = test(test_token, 5, check1, check2, check3, check4, check5)
     print("Token = check:    ", result)
 
 
-test_routine()
-
-
-# print("pretest")
-# pairing(P, Q+R) = pairing(P,Q) * pairing(P,R) - correct
+# test_routine()
+# add e.g. sort='cumulative' to sort the output
+cProfile.run('test_routine()', sort='cumulative')
